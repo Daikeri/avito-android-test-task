257d7200d65e547ca53b77d3afc756aec63a0bf7
Daikeri <daikerikot@yandex.ru>
2025-11-19 22:17:19 +0300

    добавление зависимостей для hilt+dagger, firebase-auth, сериализации

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,11 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    // аннотации
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    // hilt + dagger
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    // экосистема firebase
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
+
--- data/firebaseauth/build.gradle.kts
+++ data/firebaseauth/build.gradle.kts
@@ -1,27 +1,23 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    // сериализация для навигации
-    kotlin("plugin.serialization") version "2.0.21"
     // аннотации
     id("com.google.devtools.ksp")
     // hilt + dagger
     id("com.google.dagger.hilt.android")
+    //firebase
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.avitotask"
+    namespace = "com.example.firebaseauth"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.avitotask"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,31 +36,22 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
     //hilt + navigation
     implementation("com.google.dagger:hilt-android:2.56.2")
     ksp("com.google.dagger:hilt-android-compiler:2.56.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // firebase auth
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
