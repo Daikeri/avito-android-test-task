ce26701ded7f43d554fbc5629471a92043794c3f
Daikeri <daikerikot@yandex.ru>
2025-11-20 10:21:02 +0300

    добавление необходимых зависимостей для корректной работы, добавление конфигурационного файла firebase. Аннотации для Hilt

--- app/src/main/java/com/example/avitotask/MainActivity.kt
+++ app/src/main/java/com/example/avitotask/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.avitotask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.auth.LoginScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            LoginScreen(
+                onLoginSuccess = {},
+                onNavigateToRegister = {}
+            )
         }
     }
 }
