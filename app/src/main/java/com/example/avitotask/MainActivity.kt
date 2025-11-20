591c0b097989b3382adf6c641c7dbd7b4a3de963
Daikeri <daikerikot@yandex.ru>
2025-11-20 12:23:31 +0300

    реализация главного компонента навигации: при наличия токена авторизации осуществляем переход сразу к главному контентному экрану

--- app/src/main/java/com/example/avitotask/MainActivity.kt
+++ app/src/main/java/com/example/avitotask/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.auth.LoginScreen
+import com.example.avitotask.ui.AvitoTaskTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,10 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen(
-                onLoginSuccess = {},
-                onNavigateToRegister = {}
-            )
+            MainNavHost()
         }
     }
 }
